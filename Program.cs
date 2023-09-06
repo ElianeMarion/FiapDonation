a5ac6d1cea5612ea46ae53ed6f42dbf753d81e18
Eliane Marion <eliane@fiap.com.brgit config --global user.name eliane>
2023-09-06 18:56:32 -0300

    Fiap Donation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FiapDonationSystem
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string nome, telefone, email, senha, apelido;
+			Console.WriteLine("FIAP Donation System!");
+			//char letra = 'e';
+
+			//Criar um menu
+			Console.WriteLine("\n 1 - Registrar um novo usuário\n 2 - Logar\n 3 - Sair\n Digite a opção desejada: ");
+			int opcao = Convert.ToInt32(Console.ReadLine());
+
+			
+				Console.WriteLine("Digite seu nome: ");
+				nome = Console.ReadLine();
+				Console.WriteLine("Digite seu telefone: ");
+				telefone = Console.ReadLine();
+				Console.WriteLine("Digite seu email: ");
+				email = Console.ReadLine();
+				Console.WriteLine("Digite sua senha: ");
+				senha = Console.ReadLine();
+				Console.WriteLine("Digite seu apelido: ");
+				apelido = Console.ReadLine();
+		
+
+
+
+		}
+	}
+}
